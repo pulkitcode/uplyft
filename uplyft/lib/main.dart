a64dbab0a78fcb84d947523dfc056e7923063a65
PrathyushPRao <jay@prashy.com>
2020-09-27 01:21:31 +0530

    clean up

---
+++ uplyft/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import '../../_shared/lib/env.dart';
+import 'demo.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  static String _pkg = "gooey_edge";
+  static String get pkg => Env.getPackage(_pkg);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Chat',
+      theme: (ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Color(0xFFb5b3fd),
+      )),
+      home: GooeyEdgeDemo(title: 'Uplyft'),
+    );
+  }
+}
